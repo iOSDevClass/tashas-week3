a238750c943bc9ef0f9340834c0a193a99191f87
Tatiana Jamison <jamisont@vt.edu>
2015-10-14 10:34:42 -0700

    Initial Commit

---
+++ tableOfColors/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tableOfColors
+//
+//  Created by Tatiana Jamison on 10/14/15.
+//  Copyright © 2015 Bit Lore Institute. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ tableOfColors/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  tableOfColors
+//
+//  Created by Tatiana Jamison on 10/14/15.
+//  Copyright © 2015 Bit Lore Institute. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
